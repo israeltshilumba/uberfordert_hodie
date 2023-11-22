6c2946e360310640c4d8f7efb2aa6c3ec5ca947c
“israeltshilumbaFHTW” <israel.tshilumba.mail@gmail.com>
2023-11-22 22:24:07 +0100

    initial commit
    init

---
+++ uberfordert-hoodie/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" class="">
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+/* Add any additional styles here */
+</style>
---
+++ uberfordert-hoodie/src/main.js
@@ -0,0 +1,7 @@
+// src/main.js
+import 'tailwindcss/tailwind.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+
+createApp(App).mount('#app')
